850eb43d84c4b9d070e237fe8ac43ae1eae0908d
fangxiaoyu1900@126.com <fangxiaoyu1900@126.com>
2019-01-24 22:02:57 +0800

    test1

--- src/main/java/com/test/Test.java
+++ src/main/java/com/test/Test.java
@@ -6,6 +6,7 @@ package com.test;
  */
 public class Test {
     public static void main(String[] args) {
+        System.out.println("this is test1");
         System.out.println("hello world!!!!");
     }
 }
